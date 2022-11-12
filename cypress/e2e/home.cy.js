df9e6663438bc06c3b07c19fc1d835e1a9d960ac
Diego Gonzales de Andrade <diegogonzales@outlook.com.br>
2022-11-12 11:52:53 -0300

    Ajuste de script

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,8 @@
+import signup from '../pages/SignupPage';
+
 describe('home page', () => {
   it('app deve estar online', () => {
+    signup.go();
     cy.get('#page-home main h1').should(
       'have.text',
       'Seja um parceiro entregador pela Buger Eats',
